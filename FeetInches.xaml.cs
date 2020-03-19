d30f0d439d164a031d40e66869fd4a1ba74b61f1
John Livermore <jlivermore@bigwavesoftware.com>
2020-03-19 13:10:10 -0500

    initial

---
+++ FeetInches.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UWPFeetInches
+{
+    public sealed partial class FeetInches : UserControl
+    {
+        public FeetInches()
+        {
+            this.InitializeComponent();
+        }
+
+        #region Feet
+        public int Feet
+        {
+            get { return (int)GetValue(FeetProperty); }
+            set { SetValue(FeetProperty, value); }
+        }
+
+        public static readonly DependencyProperty FeetProperty = DependencyProperty.Register(nameof(Feet), typeof(int), typeof(FeetInches), new PropertyMetadata(0, OnPropertyChanged));
+        #endregion
+
+        #region Inches
+        public decimal Inches
+        {
+            get { return (decimal)GetValue(InchesProperty); }
+            set { SetValue(InchesProperty, value); }
+        }
+
+        public static readonly DependencyProperty InchesProperty = DependencyProperty.Register(nameof(Inches), typeof(decimal), typeof(FeetInches), new PropertyMetadata(0M, OnPropertyChanged));
+        #endregion
+
+        #region Value
+        public decimal? Value
+        {
+            get
+            {
+                return Feet * 12 + Inches;
+            }
+            set
+            {
+                Feet = value.HasValue ? (int)(value.Value / 12M) : 0;
+                Inches = value.HasValue ? value.Value - (Feet * 12M) : 0M;
+            }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(int), typeof(FeetInches), new PropertyMetadata(null));
+        #endregion
+
+        private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPFeetInches
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.ViewModel = new InchesViewModel();
+        }
+
+        public InchesViewModel ViewModel { get; set; }
+    }
+
+    public class InchesViewModel : INotifyPropertyChanged
+    {
+        private decimal? _Inches { get; set; }
+        public decimal? Inches
+        {
+            get { return _Inches; }
+            set
+            {
+                if (value != _Inches)
+                {
+                    _Inches = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Inches)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
